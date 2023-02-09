8785152dc4261f460634d0b4d9b4de48871cf107
Julian Hundeloh <julian@hundeloh-consulting.ch>
2023-02-09 16:58:25 +0000

    feat: add grant method

--- src/Podium.php
+++ src/Podium.php
@@ -6,19 +6,33 @@ use Illuminate\Support\Facades\DB;
 
 class Podium
 {
-  public function getSchema() {
+  public function getSchema()
+  {
     return 'podium';
   }
 
-  public function rebalance(string $tableSchema, string $tableName, string $targetName, int $bucket) {
+  public function grant(string $role)
+  {
+    collect([
+      'GRANT USAGE ON SCHEMA %1\$s TO %2\$s',
+      'GRANT SELECT ON TABLE %1\$s.definitions TO %2\$s'
+    ])->each(fn (string $statement) => DB::statement(sprintf($statement, Podium::getSchema(), $role)));
+  }
+
+  public function ungrant(string $role)
+  {
+  }
+
+  public function rebalance(string $tableSchema, string $tableName, string $targetName, int $bucket)
+  {
     DB::statement(
       sprintf(
         <<<SQL
 SELECT podium.rebalance(:tableSchema, :tableName, :targetName, :bucket);
-SQL
-        ,
+SQL,
         $this->getSchema()
-      ), compact('tableSchema', 'tableName', 'targetName', 'bucket')
+      ),
+      compact('tableSchema', 'tableName', 'targetName', 'bucket')
     );
   }
 }
